ed806a7b9e4645aa8887aef7c4dce9f3ffe134f6
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-07-02 22:15:32 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,22 +3,6 @@ const refs = {
   span: document.querySelector('#name-output'),
 };
 
-// const input = document.querySelector('#name-input');
-// console.log(input);
-// const span = document.querySelector('#name-output');
-// console.log(span);
-
-// refs.input.addEventListener('focus', onInputFocus);
-// refs.input.addEventListener('blur', onInputBlur);
-
-// function onInputFocus() {
-//   console.log('focus');
-// }
-
-// function onInputBlur() {
-//   console.log('blur');
-// }
-
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
