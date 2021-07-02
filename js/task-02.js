75bf2fbc621600a6cfb1012b7d4bbf9296393612
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-07-02 19:51:28 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,6 @@
+/*Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, 
+после чего вставит все li за одну операцию в список ul.ingredients*/
+
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -6,3 +9,32 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+/************ 1 */
+const ingredientsListEl = document.querySelector('#ingredients');
+console.log(ingredientsListEl);
+
+// const elements = ingredients.map(ingredient => {
+//   const ingredientsItemEl = document.createElement('li');
+//   ingredientsItemEl.textContent = ingredient;
+
+//   return ingredientsItemEl;
+// });
+
+// console.log(elements);
+// ingredientsListEl.append(...elements);
+
+// console.log(ingredientsListEl);
+
+/************ 2 */
+const createIngredientsList = ingredient => {
+  return ingredient.map(ingredient => {
+    const ingredientsItemEl = document.createElement('li');
+    ingredientsItemEl.textContent = ingredient;
+
+    return ingredientsItemEl;
+  });
+};
+
+const elements = createIngredientsList(ingredients);
+ingredientsListEl.append(...elements);
