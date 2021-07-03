b38aa867fcb25591c37122ea78452a5f11005964
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-07-03 19:53:49 +0200

    Update task-08.js

---
+++ js/task-08.js
@@ -0,0 +1,25 @@
+/*         Ще працюю над вирішенням */
+
+// const refs = {
+//   wrapper: document.querySelector('#controls'),
+//   input: document.querySelector('[type = "number"]'),
+//   renderBtn: document.querySelector('[data-action="render"]'),
+//   destroyBtn: document.querySelector('[data-action="destroy"]'),
+// };
+
+// console.log(refs.wrapper);
+// console.log(refs.input);
+// console.log(refs.renderBtn);
+// console.log(refs.destroyBtn);
+
+// // refs.input.addEventListener('input', onInputNumber);
+
+// // function onInputNumber() {
+// //   console.log(refs.input.value);
+// // }
+
+// refs.renderBtn.addEventListener('click', createBoxes);
+
+// function createBoxes() {
+//   console.log(refs.input.value);
+// }
